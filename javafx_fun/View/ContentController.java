3aec6c34f369ae392c2c9bb324e392d255c5c81c
MW5 <mwojcik1988@gmail.com>
2017-03-12 15:27:44 +0100

    just do it

--- javafx_fun/View/ContentController.java
+++ javafx_fun/View/ContentController.java
@@ -1,16 +1,26 @@
 package javafx_fun.View;
 
+import com.sun.scenario.effect.ImageData;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 import javafx_fun.JavaFx_fun;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 
 /**
@@ -21,8 +31,14 @@ import javafx_fun.JavaFx_fun;
 public class ContentController implements Initializable {
     @FXML
     private Canvas canvas;
+    @FXML
+    public Button beginBtn;
     private GraphicsContext gc;
-    private Circle target;
+    private Circle target1;
+    private Circle target2;
+    private Circle target3;
+    private Circle target4;
+    private ArrayList<Circle> targetList = new ArrayList<Circle>();
     private JavaFx_fun main;
     
     public void setMain(JavaFx_fun main) {
@@ -30,41 +46,96 @@ public class ContentController implements Initializable {
     }
     
     public void begin() {
+        beginBtn.setDisable(true);
+        beginBtn.setVisible(false);
         prepareGraphicsContext();
         handleMouse();
-        prepareTarget();
+        prepareTargets();
     }
     
     private void prepareGraphicsContext() {
         gc = canvas.getGraphicsContext2D();
     }
     
+    private boolean collChecker(MouseEvent me, Circle target) {
+        if (me.getX()>=target.getCenterX()-target.getRadius()/2 &&
+                me.getX()<=target.getCenterX()+target.getRadius()/2 &&
+                me.getY()>=target.getCenterY()-target.getRadius()/2 &&
+                me.getY()<=target.getCenterY()+target.getRadius()/2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
     private void handleMouse() {
         canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle (MouseEvent me) {
-                gc.fillOval(me.getX(), me.getY(), 5, 5); //draws from top left!
-                if( me.getX()>=target.getCenterX()-target.getRadius()/2 &&
-                    me.getX()<=target.getCenterX()+target.getRadius()/2 &&
-                    me.getY()>=target.getCenterY()-target.getRadius()/2 &&
-                    me.getY()<=target.getCenterY()+target.getRadius()/2) {
+                if (collChecker(me,target1)) {
+                    gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
                     System.out.println("hit");
+                    for (Circle target : targetList) {
+                        updateTarget(target);
+                    }
                     main.setPoints(main.getPoints()+1);
                 }
             }
         });
     }
-    
-    private void prepareTarget() {
-        target = new Circle();
-        target.setCenterX(100);
-        target.setCenterY(100);
-        target.setRadius(70);
-        gc.strokeOval(target.getCenterX()-target.getRadius()/2, target.getCenterY()-target.getRadius()/2
-                , target.getRadius(), target.getRadius());
-        gc.drawImage(new Image("file:dancer.gif", true),150,150,150,150); //doesn`t work for some reason
+    private int randVal(String orientation) {
+        int rand;
+        if (orientation.equals("x")) {
+            rand = ThreadLocalRandom.current().nextInt((int)target1.getRadius()/2, (int)(800-target1.getRadius()) + 1);
+            while(rand>=target1.getCenterX()-target1.getRadius()/2 && rand<=target1.getCenterX()+target1.getRadius()/2) {
+                rand = ThreadLocalRandom.current().nextInt((int)target1.getRadius()/2, (int)(800-target1.getRadius()) + 1);
+                
+            }
+        } else {
+            rand = ThreadLocalRandom.current().nextInt((int)target1.getRadius()/2, (int)(550-target1.getRadius()) + 1);
+            while(rand>=target1.getCenterY()-target1.getRadius()/2 && rand<=target1.getCenterY()+target1.getRadius()/2) {
+                rand = ThreadLocalRandom.current().nextInt((int)target1.getRadius()/2, (int)(550-target1.getRadius()) + 1);
+            }
+        }
+        
+        return rand;
     }
     
-
+    private void prepareTargets() {
+        target1 = new Circle();
+        target1.setRadius(100);
+        targetList.add(target1);
+        
+        target2 = new Circle();
+        target2.setRadius(75);
+        targetList.add(target2);
+        
+        target3 = new Circle();
+        target3.setRadius(50);
+        targetList.add(target3);
+        
+        target4 = new Circle();
+        target4.setRadius(25);
+        targetList.add(target4);
+        
+        for (Circle target : targetList) {
+            updateTarget(target);
+        }
+    }
+    
+    private void updateTarget(Circle target) {
+        if (target.equals(target1)) {
+            target.setCenterX(randVal("x"));
+            target.setCenterY(randVal("y"));;
+        } else {
+            target.setCenterX(target1.getCenterX());
+            target.setCenterY(target1.getCenterY());;
+        }
+        drawTarget(target);
+    }
+    private void drawTarget(Circle target) {
+        gc.strokeOval(target.getCenterX()-target.getRadius()/2, target.getCenterY()-target.getRadius()/2
+            , target.getRadius(), target.getRadius());
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
